c1e3a652c1c1d9d1146d7be054aa9405f7477634
NikolVasileva <nikol.vasileva9@gmail.com>
2026-01-25 15:32:21 +0200

    Add logo icon to home screen

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,10 +1,23 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView } from "react-native-web";
 
 export default function HomeScreen() {
     return (
-            <View >
-                <Text>Test</Text>
-            </View>
+        <SafeAreaProvider>
+            <SafeAreaView>
+                <ScrollView >
+                    <View style={{alignItems: "center"}}>
+                    <Image source={require("../../assets/logo-icon.png")}
+                            style={{
+                                width: 33,
+                                height: 35,
+                                margin: 25,
+                            }}
+                        />
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+        </SafeAreaProvider>
     );
 }
