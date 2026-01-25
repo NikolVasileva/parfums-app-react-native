32109b2535512dbc0a4d3e4a8aa65603eb1b7df2
NikolVasileva <nikol.vasileva9@gmail.com>
2026-01-25 16:15:32 +0200

    Add status bar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-// import RootNavigator from './navigation/RootNavigator.jsx';
-// import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-// import { NavigationContainer } from '@react-navigation/native';
-// import StartScreen from './screens/StartScreen.jsx';
-
-
-
-// export default function App() {
-//   return (
-
-//     <NavigationContainer>
-//       <StartScreen />
-//       <RootNavigator />
-//     </NavigationContainer>
-//   );
-// }
-
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './navigation/RootNavigator';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar style="auto" />
       <RootNavigator/>
     </NavigationContainer>
   );
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,9 +1,10 @@
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { ScrollView } from "react-native-web";
+import { StatusBar } from "expo-status-bar";
+import { ScrollView } from "react-native";
 import BlackButton from "../components/BlackButton";
 
-export default function HomeScreen({style}) {
+export default function HomeScreen({ style }) {
     return (
         <SafeAreaProvider>
             <SafeAreaView>
@@ -21,13 +22,13 @@ export default function HomeScreen({style}) {
 
                     {/* Best Sellerts Section */}
                     <View>
-                        <View style={{flexDirection: "row", justifyContent: "space-between"}}>
+                        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
                             <View style={{ gap: 10 }}>
                                 <Text style={{ fontSize: 24, color: "#262825", fontWeight: "medium" }}>Best Sellers</Text>
                                 <Text style={{ fontSize: 16, color: "#808080" }}>The Best Parfume Ever</Text>
                             </View>
-                            <View style={{width: 100}}>
-                                <BlackButton title="see all >" style={{paddingHorizontal: 12, paddingVertical: 6}}/>
+                            <View style={{ width: 100 }}>
+                                <BlackButton title="see all >" style={{ paddingHorizontal: 12, paddingVertical: 6 }} />
                             </View>
                         </View>
                     </View>
