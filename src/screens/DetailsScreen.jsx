ffaba184c32a7aa9954faef2b3d6cfd9c4674347
NikolVasileva <nikol.vasileva9@gmail.com>
2026-01-27 22:05:31 +0200

    Add header section to the details screen

--- src/screens/DetailsScreen.jsx
+++ src/screens/DetailsScreen.jsx
@@ -1,12 +1,47 @@
-import { Text, View } from "react-native";
+import { Image, Pressable, ScrollView, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function DetailsScreen({route}) {
 
     const { parfumId } = route.params;
 
+    const arrowPressHandler = () => {
+        navigation.navigate("HomeScreen")
+    };
+
     return(
-        <View>
-            <Text>Test {parfumId}</Text>
-        </View>
+        <SafeAreaProvider>
+            <SafeAreaView style={{ backgroundColor: "#FBFBFB" }} >
+                <ScrollView style={{ padding: 15, }}>
+                    {/* Header Section */}
+                    <View style={{ alignItems: "center", flexDirection: "row", justifyContent: "space-between" }}>
+                        <Pressable onPress={arrowPressHandler}>
+                            <Image source={require("../../assets/arrow.png")}
+                                style={{
+                                    width: 33,
+                                    height: 35,
+                                    margin: 25,
+                                }}
+                            />
+                        </Pressable>
+                        <Image source={require("../../assets/logo-icon.png")}
+                            style={{
+                                width: 33,
+                                height: 35,
+                                margin: 25,
+                            }}
+                        />
+                        <Image source={require("../../assets/share.png")}
+                            style={{
+                                width: 33,
+                                height: 35,
+                                margin: 25,
+                            }}
+                        />
+                    </View>
+
+                </ScrollView>
+            </SafeAreaView>
+        </SafeAreaProvider>
     )
 }
