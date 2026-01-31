e33c6043e6f3b052414e304fd0172385a160b9da
NikolVasileva <nikol.vasileva9@gmail.com>
2026-01-31 19:54:34 +0200

    update header static -> Best Sellers Screen

--- src/screens/BestSellertsScreen.jsx
+++ src/screens/BestSellertsScreen.jsx
@@ -5,7 +5,7 @@ import { parfums } from "../data/parfums";
 import ParfumCard from "../components/ParfumCard";
 
 
-export default function BestSellersScreen({navigation}) {
+export default function BestSellersScreen({ navigation }) {
     const bestSellers = parfums.filter(item => item.isBestSeller);
 
     const arrowPressHandler = () => {
@@ -19,33 +19,33 @@ export default function BestSellersScreen({navigation}) {
     return (
         <SafeAreaProvider>
             <SafeAreaView style={{ backgroundColor: "#FBFBFB", flex: 1 }} >
-                <ScrollView style={{ padding: 10, }}>
-                    {/* Header Section */}
-                    <View style={{ alignItems: "center", flexDirection: "row", justifyContent: "space-between" }}>
-                        <Pressable onPress={arrowPressHandler}>
-                            <Image source={require("../../assets/arrow.png")}
-                                style={{
-                                    width: 33,
-                                    height: 35,
-                                    margin: 25,
-                                }}
-                            />
-                        </Pressable>
-                        <Image source={require("../../assets/logo-icon.png")}
+                {/* Header Section */}
+                <View style={{ alignItems: "center", flexDirection: "row", justifyContent: "space-between" }}>
+                    <Pressable onPress={arrowPressHandler}>
+                        <Image source={require("../../assets/arrow.png")}
                             style={{
                                 width: 33,
                                 height: 35,
                                 margin: 25,
                             }}
                         />
-                        <Image source={require("../../assets/share.png")}
-                            style={{
-                                width: 33,
-                                height: 35,
-                                margin: 25,
-                            }}
-                        />
-                    </View>
+                    </Pressable>
+                    <Image source={require("../../assets/logo-icon.png")}
+                        style={{
+                            width: 33,
+                            height: 35,
+                            margin: 25,
+                        }}
+                    />
+                    <Image source={require("../../assets/share.png")}
+                        style={{
+                            width: 33,
+                            height: 35,
+                            margin: 25,
+                        }}
+                    />
+                </View>
+                <ScrollView style={{ paddingHorizontal: 10, }}>
                     <View style={{ gap: 15, padding: 5 }}>
                         <Text style={{ fontSize: 24, color: "#262825", fontWeight: "medium", alignItems: "center", textAlign: "center" }}>Best Sellers</Text>
                         <Text style={{ fontSize: 16, color: "#808080", alignItems: "center", textAlign: "center" }}>The Best Parfume Ever</Text>
@@ -56,7 +56,7 @@ export default function BestSellersScreen({navigation}) {
                         <View style={{ flexDirection: "row", justifyContent: "space-between", flexWrap: "wrap" }}>
                             {bestSellers.map((item) => (
                                 <View style={{ width: "48%", marginBottom: 20 }} key={item.id}>
-                                    <ParfumCard {...item} onPress={parfumCardPressedHandler}/>
+                                    <ParfumCard {...item} onPress={parfumCardPressedHandler} />
                                 </View>
                             ))}
                         </View>
